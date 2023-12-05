a394f8df66ed7ff1322af1269ba97ebb8c812c22
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-12-06 01:26:01 +0530

    helloweb

---
+++ config/plugins.js
@@ -0,0 +1,58 @@
+module.exports = ({ env }) => ({
+   // ...
+     'import-export-entries': {
+    enabled: true,
+    config: {
+      // See `Config` section.
+    },
+  },
+   upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+   },
+   // ...
+  // ...
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: [ "phoneno" , "deliveryaddress" ],
+      },
+    },
+  },
+  // ...
+ // ...
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        service: 'gmail' ,
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        
+      },
+      settings: {
+        defaultFrom: 'assipl023@gmail.com',
+        defaultReplyTo: 'acrotechllp@gmail.com',
+      },
+    },
+  },
+  // ...
+   
+
+  });
+
+
+  
